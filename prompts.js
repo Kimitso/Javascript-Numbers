5988caa03b7aabc2748604036f71f2d8fcb0a7ac
Tobias <xxshadowwolfxx@icloud.com>
2021-07-28 14:12:10 -0400

    main

---
+++ prompts.js
@@ -0,0 +1,8 @@
+var userInput1 = prompt("Select any number");
+var userInput2 = prompt("Select any number");
+
+var addition = +userInput1 + +userInput2;
+var subtraction = +userInput1 + +userInput2;
+var mutliplication = +userInput1 + +userInput2;
+var division = +userInput1 + +userInput2;
+var exponent = 
